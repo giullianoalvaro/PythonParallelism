4fcb409f8f19aef4a320bf505a23e99f73972eee
Giulliano Álvaro Andrade Pinheiro <giullianoalvaro@outlook.com>
2019-04-25 14:57:31 -0300

    Analysis of processed items and calculation of average processing

--- Timeit.py
+++ Timeit.py
@@ -1,26 +1,38 @@
 #! /usr/bin/env python
 # encoding:utf-8
-from PIL import Image
+import time
 
-# try:
-#     from PIL import Image
-# except ImportError as e:
-#     print(e.msg)
-#     raise ImportError(e)
+for x in range (0,5):  
+    b = "Loading" + "." * x
+    print (b, end="\r")
+    time.sleep(1)
 
 
-# try:
-#     from PIL.Image import Image
-# except e:
-#     print(e)
 
-# image_path = '/path/to/image'
-# image = Image.open(image_path)
-# count = 0
 
-# image.getdata() returns all the pixels in the image
-for pixel in image.getdata():
-    if pixel == (255, 255, 255):
-        count += 1
+# #! /usr/bin/env python
+# # encoding:utf-8
+# from PIL import Image
 
-print(count)
+# # try:
+# #     from PIL import Image
+# # except ImportError as e:
+# #     print(e.msg)
+# #     raise ImportError(e)
+
+
+# # try:
+# #     from PIL.Image import Image
+# # except e:
+# #     print(e)
+
+# # image_path = '/path/to/image'
+# # image = Image.open(image_path)
+# # count = 0
+
+# # image.getdata() returns all the pixels in the image
+# for pixel in image.getdata():
+#     if pixel == (255, 255, 255):
+#         count += 1
+
+# print(count)
--- rainbow-vision.py
+++ rainbow-vision.py
@@ -1,12 +1,15 @@
 import time
+import datetime
 import colorsys
 import numpy as np
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.pyplot as plt
 from PIL import Image
+from time import strftime
 from time import time as t
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
 
 # (1) Import the file to be analyzed!
+#img_file = Image.open("dead_parrot.jpg")
 img_file = Image.open("thedress.jpg")
 img = img_file.load()
 
@@ -17,36 +20,49 @@ hues = {}
 
 print("Total X: %d" % xs)
 print("Total Y: %d" % ys)
-time.sleep(3)
 
+begin = datetime.datetime.now()
+medium_time = datetime.datetime.now()
+medium_time -= medium_time
 
-begin = time.ctime()
 # (3) Examine each pixel in the image file
 for x in range(0, xs):
-  for y in range(0, ys):
-    print("Analyse x: %d | y: %d" % (x, y))
-    # (4)  Get the RGB color of the pixel
-    [r, g, b] = img[x, y]
+    for y in range(0, ys):
+        #print("Analyse x: %d | y: %d" % (x, y))
+        start = datetime.datetime.now()
+        # (4)  Get the RGB color of the pixel
+        [r, g, b] = img[x, y]
 
-    # (5)  Normalize pixel color values
-    r /= 255.0
-    g /= 255.0
-    b /= 255.0
+        # (5)  Normalize pixel color values
+        r /= 255.0
+        g /= 255.0
+        b /= 255.0
 
-    # (6)  Convert RGB color to HSV
-    [h, s, v] = colorsys.rgb_to_hsv(r, g, b)
+        # (6)  Convert RGB color to HSV
+        [h, s, v] = colorsys.rgb_to_hsv(r, g, b)
 
-    # (7)  Marginalize s; count how many pixels have matching (h, v)
-    if h not in hues:
-      hues[h] = {}
-    if v not in hues[h]:
-      hues[h][v] = 1
-    else:
-      if hues[h][v] < max_intensity:
-        hues[h][v] += 1
-end = time.ctime()
+        # (7)  Marginalize s; count how many pixels have matching (h, v)
+        if h not in hues:
+            hues[h] = {}
+        if v not in hues[h]:
+            hues[h][v] = 1
+        else:
+            if hues[h][v] < max_intensity:
+                hues[h][v] += 1
+
+        end = datetime.datetime.now()
+        medium_time = (medium_time + (end - start)) / 2
+
+        b = "Analyse x: %d | y: %d" % (x, y)
+        m = "Average time measured: %s" % medium_time
+        print (b + "\t" + m, end="\r")
+        #print (m, end="\r")
+        
+
+end = datetime.datetime.now()
 print('Before: %s' % begin)
 print('After: %s' % end)
+print('Average time measured: %s' % medium_time)
 
 # (8)   Decompose the hues object into a set of one dimensional arrays we can use with matplotlib
 h_ = []
@@ -54,13 +70,14 @@ v_ = []
 i = []
 colours = []
 
+
 for h in hues:
-  for v in hues[h]:
-    h_.append(h)
-    v_.append(v)
-    i.append(hues[h][v])
-    [r, g, b] = colorsys.hsv_to_rgb(h, 1, v)
-    colours.append([r, g, b])
+    for v in hues[h]:
+        h_.append(h)
+        v_.append(v)
+        i.append(hues[h][v])
+        [r, g, b] = colorsys.hsv_to_rgb(h, 1, v)
+        colours.append([r, g, b])
 
 # (9)   Plot the graph!
 fig = plt.figure()
